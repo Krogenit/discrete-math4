b27f9f252f43a4a382b150745d42618a4169b108
Krogenit <tenkrg@gmail.com>
2020-06-18 21:07:37 +0300

    init

---
+++ DizMat4/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿using System.Drawing;
+namespace DizMat4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(290, 357);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Составить заданную схему";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(290, 427);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(224, 44);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Составить случайную схему";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.random_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(285, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(229, 200);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Решебник задачи №4\r\n\r\n\r\n\r\n\r\n\r\n\r\nВведите условие:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(0, 545);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(229, 200);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Krogenit|Илья (c) 2016";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(290, 301);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(224, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "DezMat4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1, label2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ DizMat4/Form1.cs
@@ -0,0 +1,144 @@
+﻿using DeezMat4;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DizMat4
+{
+    public class GameRunner
+    {
+        private int a, b, c;
+        public GameRunner(int a, int b, int c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public void runGame()
+        {
+            try
+            {
+                using (Core game = new Core(a, b, c))
+                {
+                    game.Run();
+                }
+            }
+            catch (Exception ex)
+            {
+                //System.Windows.Forms.MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+    public partial class Form1 : Form
+    {
+        private int a, b, c;
+        Thread oThread;
+        public Form1()
+        {
+            InitializeComponent();
+            //textBox1.Text = "3x-3y+1";
+            //readText();
+            //GameRunner gr = new GameRunner(a, b, c);
+            //Thread oThread = new Thread(new ThreadStart(gr.runGame));
+            //oThread.Start();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            readText();
+            if (oThread != null)
+                oThread.Abort();
+            GameRunner gr = new GameRunner(a, b, c);
+            oThread = new Thread(gr.runGame);
+            oThread.Start();
+            //Close();
+        }
+        private void random_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            a = int.Parse(toDouble(r.Next(10) - 5));
+            b = int.Parse(toDouble(r.Next(10) - 5));
+            c = int.Parse(toDouble(r.Next(10) - 5));
+            if (oThread != null)
+                oThread.Abort();
+            GameRunner gr = new GameRunner(a, b, c);
+            oThread = new Thread(gr.runGame);
+            oThread.Start();
+            //Close();
+        }
+        private string toDouble(int x)
+        {
+            if (x < 0) { return "-" + Convert.ToString(x * -1, 2); }
+            else { return Convert.ToString(x, 2); }
+        }
+
+        private int toTen(string x)
+        {
+            if (x[0].Equals('-'))
+            {
+                string s1 = x.Substring(1, x.Length - 1);
+                return Convert.ToInt32(s1, 2) * -1;
+            }
+            else
+            {
+                return Convert.ToInt32(x, 2);
+            }
+        }
+        private void readText()
+        {
+            try
+            {
+                string s = textBox1.Text;
+                int i = 0;
+                string temp = "";
+                while (!s[i].Equals('x'))
+                {
+                    temp += s[i];
+                    i += 1;
+                }
+                if (i != 0)
+                {
+                    a = int.Parse(toDouble(int.Parse(temp)));
+                }
+                else
+                    a = 1;
+                i += 1;
+                temp = "";
+                while (!s[i].Equals('y'))
+                {
+                    temp += s[i];
+                    i += 1;
+                }
+                if (i != 2)
+                {
+                    b = int.Parse(toDouble(int.Parse(temp)));
+                }
+                else
+                {
+                    if (s[i - 1].Equals('-'))
+                        b = -1;
+                    else
+                        b = 1;
+                }
+                i += 1;
+                temp = "";
+                while (i < s.Length)
+                {
+                    temp += s[i];
+                    i += 1;
+                }
+                c = int.Parse(toDouble(int.Parse(temp)));
+            }
+            catch (Exception ex)
+            {
+                textBox1.Text = "Неправильная формула";
+            }
+        }
+    }
+}
